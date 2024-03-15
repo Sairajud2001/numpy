fac483072aaf81682760de1e65d861fd0532bfce
sai kiran <dommarajusai.kiran@ad.infosys.com>
2024-03-15 12:51:05 +0530

    initilaization of numpy

---
+++ num.py
@@ -0,0 +1,5 @@
+import numpy as np
+a=np.array([1,2,3])
+# The `print(a)` statement is printing the contents of the numpy array `a` to the console. In this
+# case, it will print `[1 2 3]`, which are the elements of the array `a`.
+print(a)
